4fa666ca891a8774fe5c7ca9ed7123e1d5ba3378
cassiekang <143849077+cassiekang@users.noreply.github.com>
2023-09-24 06:54:11 +0000

    update matrix

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 """
 Main code
 """
+import sys
 
 
 def add(a, b):
     return a + b
 
 
+def get_python_version():
+    return sys.version
+
+
 if __name__ == "__main__":
     result = add(3, 5)
     print(f"The result of adding 3 and 5 is {result}")
+    print("Python Version:", get_python_version())
--- test_main.py
+++ test_main.py
@@ -4,6 +4,8 @@ Test goes here
 """
 
 from main import add
+import sys
+import main
 
 
 def test_add_positive_numbers():
@@ -23,3 +25,7 @@ if __name__ == "__main__":
     test_add_negative_numbers()
     test_add_mixed_numbers()
     print("All tests passed.")
+
+
+def test_get_python_version():
+    assert main.get_python_version() == sys.version
